ede13f02e9082e0f6060792c799fed2cf848af68
Pinky Baring <pinky.baring@zmtsys.com>
2021-10-27 02:10:07 +0800

    added summary comments and fixed income tax computation

--- src/routes/api/pre-income-tax.js
+++ src/routes/api/pre-income-tax.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-  
-  //with parameters
+   /**
+ * @api {get} /api/calculate-pre-tax-income-from-take-home Compute Base Salary
+ * @apiName calculate-pre-tax-income-from-take-home
+ * @apiGroup api
+ * @apiParam {float} postTaxSalary.
+ * @apiSuccess (Success 200) {Object} result computation
+ */
   router.get('/', (req, res) => {
   
     //accessing get parameters
@@ -20,84 +25,72 @@ const router = express.Router();
 
     };
 
-    //computations
-    let annuation = computeSuperAnnuation(salary);
-    let incomeTax = computeIncomeTax(salary);
-    let medicareTax = computeMedicare(salary);
-    let totalTax = incomeTax + medicareTax ;
+   //computations
+   let annuation = computeSuperAnnuation(salary);
+   let incomeTax = computeIncomeTax(salary);
+   let medicareTax = computeMedicare(salary);
+   let totalTax = incomeTax + medicareTax ;
 
-    result.superannuation = annuation;
-    result.taxes.income = incomeTax;
-    result.taxes.medicare = medicareTax;
-    result.taxes.total = totalTax
-    result.postTaxIncome = salary - totalTax;
-  
-    res.json(result);
-  });
+   result.superannuation = annuation;
+   result.taxes.income = incomeTax;
+   result.taxes.medicare = medicareTax;
+   result.taxes.total = totalTax
+   result.postTaxIncome = salary - totalTax;
+ 
+   res.json(result);
+ });
 
-  function computeSuperAnnuation(salary)
-  {
+ function computeSuperAnnuation(salary)
+ {
+   let percent = 0.095;
+   return Math.round(salary * percent);
+ }
 
-    let percent = 0.095;
-    return roundToHundredth(salary * percent); //rounded to nearest cent
+ function computeIncomeTax(salary)
+ {
+   let incomeTax = 0.0
+   let last = salary;
+   let taxBracket = [
+     {bracket: 180000, tax: 0.45},
+     {bracket: 87000, tax: 0.37},
+     {bracket: 37000, tax: 0.325},
+     {bracket: 18200, tax: 0.19}, 
+     {bracket: 0, tax: 0}
+   ];
 
-  }
+   taxBracket.forEach(t => {
+     if (salary > t.bracket) {
+       let bracketComp = (last - (t.bracket + 1)) * t.tax;
+       let comp = Math.round(bracketComp);
+       console.log(comp);
+       incomeTax = incomeTax + comp;
+       last = t.bracket;
+     }      
+   });
+   
+   return incomeTax;
 
-  function computeIncomeTax(salary)
-  {
-    let percent = 0;
+ }
 
-    if (salary > 0 && salary < 18201) {
-        //0
-        percent = 0;
-    } 
-    else if (salary > 18200 && salary < 37001) {
-        //19%
-        percent = 0.19;
-    } else if(salary > 37000 && salary < 87001)
-    {
-        //32.5%       
-        percent = 0.325;
-    } else if(salary > 87000 && salary < 180001)
-    {
-        //37%        
-        percent = 0.37;
-    }      
-    else {
-      //over $180,000
-      //45 %      
-      percent = 0.45;
-    }
+ function computeMedicare(salary)
+ {
+   let percent = 0;
 
-    return salary * percent; //rounded to nearest cent
+   if (salary > 0 && salary < 21336) {
+       percent = 0;
+   } 
+   else if (salary >= 21336 && salary <= 26668) {
+       percent = 0.1;
+       salary = salary - 21336;
 
-  }
+   } else
+   {
+       percent = 0.02;
+   }
 
-  function computeMedicare(salary)
-  {
-    let percent = 0;
+   var result = salary * percent; 
+   return Math.round(result);
 
-    if (salary > 0 && salary < 21336) {
-        percent = 0;
-    } 
-    else if (salary >= 21336 && salary <= 26668) {
-        percent = 0.1;
-        salary = salary - 21336;
-
-    } else
-    {
-        percent = 0.02;
-    }
-
-    var result = salary * percent; 
-
-    return roundToHundredth(result);
-
-  }
-
-  function roundToHundredth(value)
-  {
-    return Number(value.toFixed(2));
-  }
+ }
 
   module.exports = router;
