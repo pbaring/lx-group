a77a1eca1b49a220c46c61390202f2bb064d1b0e
Pinky Baring <pinky.baring@zmtsys.com>
2021-10-27 19:37:30 +0800

    added validation

--- src/routes/api/pre-income-tax.js
+++ src/routes/api/pre-income-tax.js
@@ -11,33 +11,43 @@ const router = express.Router();
  */
   router.get('/', (req, res) => {
   
-    //accessing get parameters
-    var postSalary = parseFloat(req.query.postTaxSalary);
-    var result = {
-        baseSalary: 0.0,
-        superannuation: 0.0,
-        taxes: {
-            income: 0.0,
-            medicare: 0.0,
-            total: 0.0
-        },
-        postTaxIncome: postSalary
+    //accessing get parameters    
+    var isNumber = !isNaN(req.query.postTaxSalary);
 
-    };
+    if (isNumber) {      
+      var postSalary = parseFloat(req.query.postTaxSalary);
+      var result = {
+          baseSalary: 0.0,
+          superannuation: 0.0,
+          taxes: {
+              income: 0.0,
+              medicare: 0.0,
+              total: 0.0
+          },
+          postTaxIncome: postSalary
 
-   //computations
-   let annuation = computeSuperAnnuation(salary);
-   let incomeTax = computeIncomeTax(salary);
-   let medicareTax = computeMedicare(salary);
-   let totalTax = incomeTax + medicareTax ;
+      };
 
-   result.superannuation = annuation;
-   result.taxes.income = incomeTax;
-   result.taxes.medicare = medicareTax;
-   result.taxes.total = totalTax
-   result.postTaxIncome = salary - totalTax;
- 
-   res.json(result);
+    //computations
+    let annuation = computeSuperAnnuation(salary);
+    let incomeTax = computeIncomeTax(salary);
+    let medicareTax = computeMedicare(salary);
+    let totalTax = incomeTax + Math.round(medicareTax);
+
+    result.superannuation = annuation;
+    result.taxes.income = incomeTax;
+    result.taxes.medicare = medicareTax;
+    result.taxes.total = totalTax
+    result.postTaxIncome = salary - totalTax;
+  
+    res.json(result);
+    }
+    else
+    {
+      var msg = {message: `Invalid Number: ${req.query.postTaxSalary}`};
+      res.status(400).json(msg);
+    }
+
  });
 
  function computeSuperAnnuation(salary)
@@ -47,50 +57,51 @@ const router = express.Router();
  }
 
  function computeIncomeTax(salary)
- {
-   let incomeTax = 0.0
-   let last = salary;
-   let taxBracket = [
-     {bracket: 180000, tax: 0.45},
-     {bracket: 87000, tax: 0.37},
-     {bracket: 37000, tax: 0.325},
-     {bracket: 18200, tax: 0.19}, 
-     {bracket: 0, tax: 0}
-   ];
+  {
+    let incomeTax = 0.0
+    let ceiling = salary;
+    let taxBracket = [
+      {bracket: 180000, tax: 0.45},
+      {bracket: 87000, tax: 0.37},
+      {bracket: 37000, tax: 0.325},
+      {bracket: 18200, tax: 0.19}, 
+      {bracket: 0, tax: 0}
+    ];
 
-   taxBracket.forEach(t => {
-     if (salary > t.bracket) {
-       let bracketComp = (last - (t.bracket + 1)) * t.tax;
-       let comp = Math.round(bracketComp);
-       console.log(comp);
-       incomeTax = incomeTax + comp;
-       last = t.bracket;
-     }      
-   });
-   
-   return incomeTax;
+    taxBracket.forEach(t => {
+      if (salary > t.bracket) {
+        let bracketComp = (ceiling - (t.bracket + 1)) * t.tax;
+        console.log(bracketComp);
+        incomeTax = incomeTax + Math.round(bracketComp);
+        ceiling = t.bracket;
+      }      
+    });
+    
+    return Math.round(incomeTax);
 
- }
+  }
 
- function computeMedicare(salary)
- {
-   let percent = 0;
+  function computeMedicare(salary)
+  {
+    let ceiling = salary;
+    let medicare = 0.0;
 
-   if (salary > 0 && salary < 21336) {
-       percent = 0;
-   } 
-   else if (salary >= 21336 && salary <= 26668) {
-       percent = 0.1;
-       salary = salary - 21336;
+    let taxBracket = [
+      {bracket: 26668, tax: 0.02},
+      {bracket: 21336, tax: 0.1}
+    ];
 
-   } else
-   {
-       percent = 0.02;
-   }
+    taxBracket.forEach(t => {
+      if (salary > t.bracket) {
+        let bracketComp = (ceiling - t.bracket) * t.tax;
+        console.log(bracketComp);
+        medicare = medicare + bracketComp;
+        ceiling = t.bracket;
+      }      
+    });
+    
+    return medicare < 0.159 ? medicare : Math.round(medicare);
 
-   var result = salary * percent; 
-   return Math.round(result);
-
- }
+  }
 
   module.exports = router;
